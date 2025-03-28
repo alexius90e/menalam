394a28277ab4bbe77f310e283084072270571cde
Aliaksei Karzhou <alexius90e@gmail.com>
2025-03-28 17:38:00 +0300

    fix: fixed issues

--- js/index.js
+++ js/index.js
@@ -1,26 +1,4 @@
-const headerBurgerButton = document.querySelector('.header__burger-button');
-const headerNav = document.querySelector('.header__nav');
 
-if (headerBurgerButton && headerNav) {
-  headerBurgerButton.addEventListener('click', (event) => {
-    const isActive = event.target.classList.contains('active');
-
-    if (isActive) {
-      headerNav.classList.remove('active');
-      headerBurgerButton.classList.remove('active');
-    } else {
-      headerNav.classList.add('active');
-      headerBurgerButton.classList.add('active');
-    }
-  });
-
-  headerNav.addEventListener('click', (event) => {
-    if (event.target === event.currentTarget) {
-      headerNav.classList.remove('active');
-      headerBurgerButton.classList.remove('active');
-    }
-  });
-}
 
 const needsSwiper = new Swiper('.needs .swiper', {
   slidesPerView: 1,
@@ -96,14 +74,3 @@ const hwStep2Image = document.querySelector(
 if (hwStep2Image) {
   observer.observe(hwStep2Image);
 }
-
-window.addEventListener('scroll', () => {
-  const scrolled = window.pageYOffset;
-  const layers = document.querySelectorAll('.parallax-layer');
-
-  layers.forEach((layer) => {
-    const speed = 0.5;
-    const yPos = -(scrolled * speed);
-    layer.style.transform = `translateY(${yPos}px)`;
-  });
-});
